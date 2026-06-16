39e6b58d820574da74e68798e6c7384919957262
Angelo Longoria <arlongoria93@gmail.com>
2026-06-16 17:06:46 -0500

    Wraps the application in a CartProvider to manage cart state and enhances the root layout structure. This change ensures that the Navbar and child components have access to the cart context, improving the overall user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import Navbar from '@/components/Navbar'
-import './globals.css'
+import Navbar from "@/components/Navbar";
+import { CartProvider } from "@/context/CartContext";
+import "./globals.css";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}) { 
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
         <title></title>
       </head>
       <body>
-        <Navbar />
-        {children}</body>
+        <CartProvider>
+          <Navbar />
+          {children}
+        </CartProvider>
+      </body>
     </html>
-  )
+  );
 }
