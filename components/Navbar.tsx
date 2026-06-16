8d1261c3276e2c9932c5f9c2fc24394d8a72b1f3
Angelo Longoria <arlongoria93@gmail.com>
2026-06-16 12:45:07 -0500

    Updates navbar with branding and store links
    Replaces generic site navigation with store-specific routes to align with the e-commerce structure.
    Adds a brand logo and a graphical shopping cart to improve the visual shopping interface.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 export default function Navbar() {
   return <nav>
+    <Image src="/images/fluxstore-logo.png" alt="Logo" width={100} height={50} />
     <Link href="/">Home</Link>
-    <Link href="/about">About</Link>
-    <Link href="/contact">Contact</Link>
+    <Link href="/products">Products</Link>
+    <Link href="/cart">Cart</Link>
+    <Link href="/cart">
+    <Image src="/images/cart-icon.svg" alt="Cart Icon" width={24} height={24} />
+    </Link>
+
   </nav>;
 }
